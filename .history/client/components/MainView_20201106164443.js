b41fe928ba84dd5d2b91788fd1743f074a86887a
Melanie Xu <66959214+melaniedxu@users.noreply.github.com>
2021-02-01 14:21:12 -0800

    Delete .history directory

--- .history/client/components/MainView_20201106164443.js
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { getUsers, joinChat, sendMessage } from '../network/getData';
-import { setupWSConnection } from '../network/notifications';
-
-function MainView() {
-
-}
-
-export default MainView;
